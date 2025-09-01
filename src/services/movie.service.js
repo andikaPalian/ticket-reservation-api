4ad4190eee2d8db1740ae8b351daeb6eca09345d
Andika Palian <andika12des@gmail.com>
2025-09-01 18:57:02 +0800

    feat: add command for explained the code function and change  parameter from prism to tx

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -5,8 +5,10 @@ import { AppError } from "../utils/errorHandler.js";
 
 const prisma = new PrismaClient();
 
+// Super Admin: Add new movie
 export const addMovie = async (adminId, files, movieData) => {
     try {
+        // Check if the admin is exists or not
         const superAdmin = await prisma.admin.findUnique({
             where: {
                 adminId: adminId
@@ -16,10 +18,12 @@ export const addMovie = async (adminId, files, movieData) => {
             throw new AppError("Theater admin not found", 404);
         }
 
+        // Check if the admin role is SUPER_ADMIN or not
         if (superAdmin.role !== "SUPER_ADMIN") {
             throw new AppError("Unauthorized: You do not have permission to add movies", 401);
         }
 
+        // Destructure movie data
         const { title, description, duration, releaseDate, genre, language, rating, director, cast, isPublished } = movieData;
 
         // Cloudinary
@@ -28,34 +32,43 @@ export const addMovie = async (adminId, files, movieData) => {
         let trailerUrl = null;
         let trailerPublicId = null;
 
+        // Upload poster to cloudinary
         if (files.posters && files.posters[0]) {
+            // Upload poster
             const result = await cloudinary.uploader.upload(files.posters[0].path, {
-                folder: 'posters',
-                resource_type: 'image',
-                use_filename: true,
-                unique_filename: true
+                folder: 'posters', // Create a folder named 'posters' in cloudinary to store poster images
+                resource_type: 'image', // Specify the resource type as image
+                use_filename: true, // Use the original filename
+                unique_filename: true // Ensure the filename is unique
             });
 
+            // Delete the file from local uploads folder
             await fs.unlink(files.posters[0].path);
 
+            // Set poster URL and public ID
             posterUrl = result.secure_url;
             posterPublicId = result.public_id;
         }
 
+        // Upload trailer to cloudinary
         if (files.trailers && files.trailers[0]) {
+            // Upload trailer
             const result = await cloudinary.uploader.upload(files.trailers[0].path, {
-                folder: 'trailers',
-                resource_type: 'video',
-                use_filename: true,
-                unique_filename: true
+                folder: 'trailers', // Create a folder named 'trailers' in cloudinary to store trailer videos
+                resource_type: 'video', // Specify the resource type as video
+                use_filename: true, // Use the original filename
+                unique_filename: true // Ensure the filename is unique
             });
 
+            // Delete the file from local uploads folder
             await fs.unlink(files.trailers[0].path);
 
+            // Set trailer URL and public ID
             trailerUrl = result.secure_url;
             trailerPublicId = result.public_id;
         };
 
+        // Handle optional fields
         const poster = posterUrl || null;
         const trailer = trailerUrl || null;
 
@@ -78,8 +91,10 @@ export const addMovie = async (adminId, files, movieData) => {
         //     }
         // });
 
-        const newMovie = await prisma.$transaction(async (prism) => {
-            const movie = await prism.movies.create({
+        // Using transaction to ensure both movie and cast are created
+        const newMovie = await prisma.$transaction(async (tx) => {
+            // Insert movie
+            const movie = await tx.movies.create({
                 data: {
                     title: title,
                     description: description,
@@ -105,7 +120,8 @@ export const addMovie = async (adminId, files, movieData) => {
                     movieId: movie.movieId
                 }));
 
-                await prism.cast.createMany({
+                // Insert cast data
+                await tx.cast.createMany({
                     data: castData
                 });
             }
@@ -120,12 +136,15 @@ export const addMovie = async (adminId, files, movieData) => {
     }
 };
 
+// Get all movies with pagination and search
 export const getAllMovies = async ({page = 1, limit = 10, search = ''}) => {
     try {
+        // Pagination calculation
         const pageNum = Number(page);
         const limitNum = Number(limit);
         const skip = (pageNum - 1) * limitNum;
 
+        // Fetch movies from database with pagination and search
         const movies = await prisma.movies.findMany({
             where: {
                 title: {
@@ -143,6 +162,7 @@ export const getAllMovies = async ({page = 1, limit = 10, search = ''}) => {
             },
         });
 
+        // Get total count of movies for pagination
         const totalMovies = await prisma.movies.count({
             where: {
                 title: {
@@ -165,8 +185,10 @@ export const getAllMovies = async ({page = 1, limit = 10, search = ''}) => {
     }
 };
 
+// Get movie by ID
 export const getMovieById = async (movieId) => {
     try {
+        // Get movie details from database
         const movie = await prisma.movies.findUnique({
             where: {
                 movieId: movieId
@@ -183,8 +205,10 @@ export const getMovieById = async (movieId) => {
     }
 };
 
+// Super Admin: Update movie
 export const updateMovie = async (adminId, movieId, files, movieData) => {
     try {
+        // Check if the admin is exists or not
         const superAdmin = await prisma.admin.findUnique({
             where: {
                 adminId: adminId
@@ -194,10 +218,12 @@ export const updateMovie = async (adminId, movieId, files, movieData) => {
             throw new AppError("Theater admin not found", 404);
         }
 
+        // Check if the admin role is SUPER_ADMIN or not
         if (superAdmin.role !== "SUPER_ADMIN") {
             throw new AppError("Unauthorized: You do not have permission to update movies", 401);
         }
 
+        // Check if the movie exists or not
         const existingMovie = await prisma.movies.findUnique({
             where: {
                 movieId: movieId
@@ -207,8 +233,10 @@ export const updateMovie = async (adminId, movieId, files, movieData) => {
             throw new AppError("Movie not found", 404);
         }
 
+        // Destructure movie data
         const { title, description, duration, releaseDate, genre, language, rating, director, cast, isPublished } = movieData;
 
+        // Prepare update data object
         const updateData = {}
 
         // Cloudinary
@@ -217,48 +245,61 @@ export const updateMovie = async (adminId, movieId, files, movieData) => {
         let trailerUrl = existingMovie.trailerUrl;
         let trailerPublicId = existingMovie.trailerPublicId;
 
+        // Upload new poster to cloudinary if provided
         if (files?.posters && files?.posters[0]) {
+            // Delete existing poster from cloudinary if exists
             if (existingMovie.posterPublicId) {
                 await cloudinary.uploader.destroy(existingMovie.posterPublicId);
             }
 
+            // Upload new poster
             const result = await cloudinary.uploader.upload(files.posters[0].path, {
-                folder: 'posters',
-                resource_type: 'image',
-                use_filename: true,
-                unique_filename: true
+                folder: 'posters', // Create a folder named 'posters' in cloudinary to store poster images
+                resource_type: 'image', // Specify the resource type as image
+                use_filename: true, // Use the original filename
+                unique_filename: true // Ensure the filename is unique
             });
 
+            // Delete the file from local uploads folder
             await fs.unlink(files.posters[0].path);
 
+            // Set new poster URL and public ID
             posterUrl = result.secure_url;
             posterPublicId = result.public_id;
 
+            // Update the update data object
             updateData.posterUrl = posterUrl;
             updateData.posterPublicId = posterPublicId;
         }
 
+        // Upload new trailer to cloudinary if provided
         if (files?.trailers && files?.trailers[0]) {
+            // Delete existing trailer from cloudinary if exists
             if (existingMovie.trailerPublicId) {
                 await cloudinary.uploader.destroy(existingMovie.trailerPublicId);
             }
 
+            // Upload new trailer
             const result = await cloudinary.uploader.upload(files.trailers[0].path, {
-                folder: 'trailers',
-                resource_type: 'video',
-                use_filename: true,
-                unique_filename: true
+                folder: 'trailers', // Create a folder named 'trailers' in cloudinary to store trailer videos
+                resource_type: 'video', // Specify the resource type as video
+                use_filename: true, // Use the original filename
+                unique_filename: true // Ensure the filename is unique
             });
 
+            // Delete the file from local uploads folder
             await fs.unlink(files.trailers[0].path);
 
+            // Set new trailer URL and public ID
             trailerUrl = result.secure_url;
             trailerPublicId = result.public_id;
 
+            // Update the update data object
             updateData.trailerUrl = trailerUrl;
             updateData.trailerPublicId = trailerPublicId;
         }
 
+        // if fields are not undefined, add them to the updateData object
         if (title !== undefined) updateData.title = title;
         if (description !== undefined) updateData.description = description;
         if (duration !== undefined) updateData.duration = Number(duration);
@@ -283,12 +324,14 @@ export const updateMovie = async (adminId, movieId, files, movieData) => {
 
             // Add new casts
             if (Array.isArray(cast)) {
+                // Map the cast array to an array of cast objects
                 const newCast = cast.map((c) => ({
                     name: c.name,
                     role: c.role,
                     movieId: existingMovie.movieId
                 }));
 
+                // Create new casts
                 await prisma.cast.createMany({
                     data: newCast
                 });
@@ -301,6 +344,7 @@ export const updateMovie = async (adminId, movieId, files, movieData) => {
         // updateData.trailerUrl = trailerUrl;
         // updateData.trailerPublicId = trailerPublicId;
 
+        // Update movie
         const updatedMovie = await prisma.movies.update({
             where: {
                 movieId: movieId
@@ -318,8 +362,10 @@ export const updateMovie = async (adminId, movieId, files, movieData) => {
     }
 };
 
+// Super Admin: Delete movie
 export const deleteMovie = async (adminId, movieId) => {
     try {
+        // Check if the admin is exists or not
         const superAdmin = await prisma.admin.findUnique({
             where: {
                 adminId: adminId
@@ -329,10 +375,12 @@ export const deleteMovie = async (adminId, movieId) => {
             throw new AppError("Theater admin not found", 404);
         }
 
+        // Check if the admin role is SUPER_ADMIN or not
         if (superAdmin.role !== "SUPER_ADMIN") {
             throw new AppError("Unauthorized: You do not have permission to delete movies", 401);
         }
 
+        // Check if the movie exists or not
         const movie = await prisma.movies.findUnique({
             where: {
                 movieId: movieId
@@ -342,28 +390,34 @@ export const deleteMovie = async (adminId, movieId) => {
             throw new AppError("Movie not found", 404);
         }
 
-        await prisma.$transaction(async (prism) => {
-            await prism.cast.deleteMany({
+        // Using transaction to delete all related data
+        await prisma.$transaction(async (tx) => {
+            // Delete all cast that are related to this movie
+            await tx.cast.deleteMany({
                 where: {
                     movieId: movieId
                 }
             });
             
+            // Delete poster from cloudinary
             if (movie.posterPublicId) {
                 await cloudinary.uploader.destroy(movie.posterPublicId);
             }
             
+            // Delete trailer from cloudinary
             if (movie.trailerPublicId) {
                 await cloudinary.uploader.destroy(movie.trailerPublicId);
             }
 
-            await prism.movieSchedules.deleteMany({
+            // Delete all movie schedules that are related to this movie
+            await tx.movieSchedules.deleteMany({
                 where: {
                     movieId: movieId
                 }
             });
 
-            await prism.movies.delete({
+            // Delete movie
+            await tx.movies.delete({
                 where: {
                     movieId: movieId
                 }
