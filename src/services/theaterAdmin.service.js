baec153497028db5dd25c88901bfb23b8b28cbaf
Andika Palian <andika12des@gmail.com>
2025-09-04 09:06:37 +0800

    feat: add command for explained the code function

--- src/services/theaterAdmin.service.js
+++ src/services/theaterAdmin.service.js
@@ -3,8 +3,10 @@ import { AppError } from "../utils/errorHandler.js";
 
 const prisma = new PrismaClient();
 
+// SUPER ADMIN: assign admin to theater for being an adnin of the theater
 export const assignAdminToTheater = async (superAdminId, theaterId, assignerId) => {
     try {
+        // Check if the super admin is exists or not
         const superAdmin = await prisma.admin.findUnique({
             where: {
                 adminId: superAdminId
@@ -14,10 +16,12 @@ export const assignAdminToTheater = async (superAdminId, theaterId, assignerId) 
             throw new AppError("Super admin not found", 404);
         }
 
+        // Validate the admin role is SUPER_ADMIN
         if (superAdmin.role !== "SUPER_ADMIN") {
             throw new AppError("Unauthorized: You do not have permission to assign admins to theaters", 401);
         }
 
+        // Check if the theater exists
         const theater = await prisma.theaters.findUnique({
             where: {
                 theaterId: theaterId
@@ -30,6 +34,7 @@ export const assignAdminToTheater = async (superAdminId, theaterId, assignerId) 
             throw new AppError("Theater not found", 404);
         }
 
+        // Check if the admin to be assigned is exists or not
         const assigner = await prisma.admin.findUnique({
             where: {
                 adminId: assignerId
@@ -39,11 +44,13 @@ export const assignAdminToTheater = async (superAdminId, theaterId, assignerId) 
             throw new AppError("Assigner not found", 404);
         }
 
+        // Check if the admin is already assigned to the theater or not
         const theaterAdmins = theater.admin.map((admin) => admin.adminId);
         if (theaterAdmins.includes(assignerId)) {
             throw new AppError("Admin already assigned to theater", 400);
         }
 
+        // Assign the admin to the theater
         await prisma.theaters.update({
             where: {
                 theaterId: theaterId
@@ -62,8 +69,10 @@ export const assignAdminToTheater = async (superAdminId, theaterId, assignerId) 
     }
 };
 
+// SUPER ADMIN: remove admin from theater
 export const removeAdminFromTheater = async (superAdminId, theaterId, adminId) => {
     try {
+        // Check if the super admin is exists or not
         const superAdmin = await prisma.admin.findUnique({
             where: {
                 adminId: superAdminId
@@ -73,10 +82,12 @@ export const removeAdminFromTheater = async (superAdminId, theaterId, adminId) =
             throw new AppError("Super admin not found", 404);
         }
 
+        // Validate the admin role is SUPER_ADMIN
         if (superAdmin.role !== "SUPER_ADMIN") {
             throw new AppError("Unauthorized: You do not have permission to remove admins from theaters", 401);
         }
 
+        // Check if the theater exists
         const theater = await prisma.theaters.findUnique({
             where: {
                 theaterId: theaterId
@@ -89,11 +100,13 @@ export const removeAdminFromTheater = async (superAdminId, theaterId, adminId) =
             throw new AppError("Theater not found", 404);
         }
 
+        // Check if the admin is already assigned to the theater or not
         const theaterAdmins = theater.admin.map((admin) => admin.adminId);
         if (!theaterAdmins.includes(adminId)) {
             throw new AppError("Admin not assigned to theater", 400);
         }
 
+        // Remove the admin from the theater
         await prisma.theaters.update({
             where: {
                 theaterId: theaterId
